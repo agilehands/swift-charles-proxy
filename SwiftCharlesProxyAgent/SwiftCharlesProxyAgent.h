2db95f7f7e428f6f76123280343972c71501c0fe
Aman Shaikh <shaikh.aman@smaato.com>
2017-05-11 12:05:25 +0200

    Initial Commit

---
+++ SwiftCharlesProxyAgent/SwiftCharlesProxyAgent.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCharlesProxyAgent.h
+//  SwiftCharlesProxyAgent
+//
+//  Created by Aman Shaikh on 11.05.17.
+//  Copyright © 2017 Shaikh Aman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCharlesProxyAgent.
+FOUNDATION_EXPORT double SwiftCharlesProxyAgentVersionNumber;
+
+//! Project version string for SwiftCharlesProxyAgent.
+FOUNDATION_EXPORT const unsigned char SwiftCharlesProxyAgentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCharlesProxyAgent/PublicHeader.h>
+
+
